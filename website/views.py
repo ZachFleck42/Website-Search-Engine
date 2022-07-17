bceea198787c68754fcbc05aac751f798cae30c3
Zachary Fleckenstein <zachfleck42@gmail.com>
2022-07-17 15:23:30 -0400

    Adds error checking for blank forms. Vastly improves readability of views.py

--- website/views.py
+++ website/views.py
@@ -6,13 +6,17 @@ from src.search_utils import runSearch
 
 def crawl(request):
     renderArguments = {}
-    
     if request.method == "POST":
+        # Check if the 'website to crawl' field was filled in properly
         websiteURL = request.POST.get('input_url')
+        if not websiteURL:
+            renderArguments['blankForm'] = 1
+            return render(request, 'crawl.html', renderArguments)
+        renderArguments['crawledURL'] = websiteURL
+        
+        # If valid input, crawl the provided website for data and store in database
         tableName = getTableName(websiteURL)
         webpageVisitCount, crawlTime = crawlWebsite(websiteURL, tableName)
-        
-        renderArguments['crawledURL'] = websiteURL
         renderArguments['webpageVisitCount'] = webpageVisitCount
         renderArguments['crawlTime'] = round(crawlTime, 2)
         
@@ -20,6 +24,7 @@ def crawl(request):
     
     
 def search(request):
+    # Define search methods available to user
     searchMethods = (
         ('Python str.count()','COUNT'),
         ('Boyer-Moore', 'BM'),
@@ -28,20 +33,41 @@ def search(request):
         ('Aho-Corasick','AC'),
     )
     
+    # Initiate renderArguments with necessary variables to display any search page
     renderArguments = {}
     renderArguments['searchMethods'] = list(searchMethods)
     renderArguments['searchableWebsites'] = getSearchableWebsites()
     renderArguments['amountOfResultsOptions'] = (10, 50, 100, 1000)
     
+    # If the user has attempted to make a search...
     if request.method == "POST":
+        # Check if the 'website to search' field was filled in
         renderArguments['searchWebsite'] = request.POST.get('input_website')
+        if not renderArguments['searchWebsite']:
+            renderArguments['noWebsite'] = 1
+            return render(request, 'search.html', renderArguments)
+        
+        # Check if the 'search method' field was filled in. Default to str.count() method if not
         renderArguments['searchMethod'] = request.POST.get('input_method')
+        if not renderArguments['searchMethod']:
+            renderArguments['searchMethod'] = "COUNT" 
         renderArguments['searchMethodName'] = ([method for method in searchMethods if method[1] == renderArguments['searchMethod']])[0][0]  # Don't even worry about it
+        
+        # Check if the 'amount of results to display' field was filled in. Deafult to 10 if not
+        renderArguments['amountOfResults'] = request.POST.get('input_amount')
+        if not renderArguments['amountOfResults']:
+            renderArguments['amountOfResults'] = 10
+        
+        # Check if the 'search term' field was filled in properly
         renderArguments['searchTerm'] = request.POST.get('input_search')
-        renderArguments['amountOfResults'] = int(request.POST.get('input_amount'))
-
+        if renderArguments['searchTerm'] == '':
+            renderArguments['noSearchTerm'] = 1
+            return render(request, 'search.html', renderArguments)
+        
+        # If all fields filled in properly, run a search with the provided arguments
         searchResults, searchTime = runSearch(renderArguments['searchWebsite'], renderArguments['searchTerm'], renderArguments['searchMethod'], renderArguments['amountOfResults'])
         
+        # Store the results of the search in arguments to be passed to the results page
         renderArguments['searchResults'] = searchResults
         renderArguments['searchTime'] = round((searchTime * 1000), 2)
         renderArguments['foundPages'] = len(searchResults)
